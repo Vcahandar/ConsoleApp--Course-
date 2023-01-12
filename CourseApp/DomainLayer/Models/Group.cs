d93a072908bae37ea833237ae47355fdbdb86075
unknown <jahandarmv@code.edu.az>
2023-01-12 14:33:50 +0400

    Start course app project

---
+++ CourseApp/DomainLayer/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Group:BaseEntity
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public DateTime Ceratdate { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
